086349bbd1d63dbe25bfb7f043adbb97dff52d4c
Afnan Riaz <l227882@lhr.nu.edu.pk>
2023-05-02 23:15:04 +0500

    copyright correction

--- Bus_Service_App/menu.h
+++ Bus_Service_App/menu.h
@@ -116,9 +116,10 @@ namespace Bus_Service_App {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::Color::White;
-			this->label1->Location = System::Drawing::Point(355, 21);
+			this->label1->Location = System::Drawing::Point(473, 26);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(120, 38);
+			this->label1->Size = System::Drawing::Size(160, 47);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Home";
 			// 
@@ -128,9 +129,10 @@ namespace Bus_Service_App {
 			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label2->ForeColor = System::Drawing::Color::White;
-			this->label2->Location = System::Drawing::Point(25, 42);
+			this->label2->Location = System::Drawing::Point(33, 52);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(60, 19);
+			this->label2->Size = System::Drawing::Size(74, 24);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Wallet:";
 			// 
@@ -140,9 +142,10 @@ namespace Bus_Service_App {
 			this->balanceLabel->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->balanceLabel->ForeColor = System::Drawing::Color::White;
-			this->balanceLabel->Location = System::Drawing::Point(97, 44);
+			this->balanceLabel->Location = System::Drawing::Point(129, 54);
+			this->balanceLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->balanceLabel->Name = L"balanceLabel";
-			this->balanceLabel->Size = System::Drawing::Size(43, 17);
+			this->balanceLabel->Size = System::Drawing::Size(55, 22);
 			this->balanceLabel->TabIndex = 2;
 			this->balanceLabel->Text = L"Rs. 0";
 			// 
@@ -152,8 +155,8 @@ namespace Bus_Service_App {
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->accountToolStripMenuItem });
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Padding = System::Windows::Forms::Padding(4, 2, 0, 2);
-			this->menuStrip1->Size = System::Drawing::Size(1028, 24);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(5, 2, 0, 2);
+			this->menuStrip1->Size = System::Drawing::Size(1371, 28);
 			this->menuStrip1->TabIndex = 3;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -164,20 +167,20 @@ namespace Bus_Service_App {
 					this->manageAccountToolStripMenuItem
 			});
 			this->accountToolStripMenuItem->Name = L"accountToolStripMenuItem";
-			this->accountToolStripMenuItem->Size = System::Drawing::Size(64, 20);
+			this->accountToolStripMenuItem->Size = System::Drawing::Size(77, 24);
 			this->accountToolStripMenuItem->Text = L"Account";
 			// 
 			// logOutToolStripMenuItem
 			// 
 			this->logOutToolStripMenuItem->Name = L"logOutToolStripMenuItem";
-			this->logOutToolStripMenuItem->Size = System::Drawing::Size(165, 22);
+			this->logOutToolStripMenuItem->Size = System::Drawing::Size(204, 26);
 			this->logOutToolStripMenuItem->Text = L"Log Out";
 			this->logOutToolStripMenuItem->Click += gcnew System::EventHandler(this, &menu::logOutToolStripMenuItem_Click);
 			// 
 			// manageAccountToolStripMenuItem
 			// 
 			this->manageAccountToolStripMenuItem->Name = L"manageAccountToolStripMenuItem";
-			this->manageAccountToolStripMenuItem->Size = System::Drawing::Size(165, 22);
+			this->manageAccountToolStripMenuItem->Size = System::Drawing::Size(204, 26);
 			this->manageAccountToolStripMenuItem->Text = L"Manage Account";
 			// 
 			// bookBtn
@@ -186,9 +189,10 @@ namespace Bus_Service_App {
 				static_cast<System::Byte>(0)));
 			this->bookBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bookBtn.Image")));
 			this->bookBtn->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->bookBtn->Location = System::Drawing::Point(263, 170);
+			this->bookBtn->Location = System::Drawing::Point(351, 209);
+			this->bookBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->bookBtn->Name = L"bookBtn";
-			this->bookBtn->Size = System::Drawing::Size(233, 65);
+			this->bookBtn->Size = System::Drawing::Size(311, 80);
 			this->bookBtn->TabIndex = 4;
 			this->bookBtn->Text = L"    Book Seats";
 			this->bookBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -200,9 +204,10 @@ namespace Bus_Service_App {
 			this->seeBtn->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->seeBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"seeBtn.Image")));
-			this->seeBtn->Location = System::Drawing::Point(543, 170);
+			this->seeBtn->Location = System::Drawing::Point(724, 209);
+			this->seeBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->seeBtn->Name = L"seeBtn";
-			this->seeBtn->Size = System::Drawing::Size(233, 65);
+			this->seeBtn->Size = System::Drawing::Size(311, 80);
 			this->seeBtn->TabIndex = 5;
 			this->seeBtn->Text = L"  See Schedules";
 			this->seeBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -214,9 +219,10 @@ namespace Bus_Service_App {
 			this->ticketsBtn->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->ticketsBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ticketsBtn.Image")));
-			this->ticketsBtn->Location = System::Drawing::Point(263, 269);
+			this->ticketsBtn->Location = System::Drawing::Point(351, 331);
+			this->ticketsBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->ticketsBtn->Name = L"ticketsBtn";
-			this->ticketsBtn->Size = System::Drawing::Size(233, 65);
+			this->ticketsBtn->Size = System::Drawing::Size(311, 80);
 			this->ticketsBtn->TabIndex = 6;
 			this->ticketsBtn->Text = L"   View Tickets";
 			this->ticketsBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -229,9 +235,10 @@ namespace Bus_Service_App {
 				static_cast<System::Byte>(0)));
 			this->cancelBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"cancelBtn.Image")));
 			this->cancelBtn->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->cancelBtn->Location = System::Drawing::Point(543, 269);
+			this->cancelBtn->Location = System::Drawing::Point(724, 331);
+			this->cancelBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->cancelBtn->Name = L"cancelBtn";
-			this->cancelBtn->Size = System::Drawing::Size(233, 65);
+			this->cancelBtn->Size = System::Drawing::Size(311, 80);
 			this->cancelBtn->TabIndex = 7;
 			this->cancelBtn->Text = L"  Cancel Tickets";
 			this->cancelBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -244,9 +251,10 @@ namespace Bus_Service_App {
 			this->customerBtn->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->customerBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"customerBtn.Image")));
-			this->customerBtn->Location = System::Drawing::Point(543, 372);
+			this->customerBtn->Location = System::Drawing::Point(724, 458);
+			this->customerBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->customerBtn->Name = L"customerBtn";
-			this->customerBtn->Size = System::Drawing::Size(233, 65);
+			this->customerBtn->Size = System::Drawing::Size(311, 80);
 			this->customerBtn->TabIndex = 8;
 			this->customerBtn->Text = L" Customer Care";
 			this->customerBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -259,9 +267,10 @@ namespace Bus_Service_App {
 			this->label3->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label3->ForeColor = System::Drawing::Color::White;
-			this->label3->Location = System::Drawing::Point(23, 18);
+			this->label3->Location = System::Drawing::Point(31, 22);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(87, 19);
+			this->label3->Size = System::Drawing::Size(105, 24);
 			this->label3->TabIndex = 9;
 			this->label3->Text = L"Signed in:";
 			// 
@@ -271,9 +280,10 @@ namespace Bus_Service_App {
 			this->nameLabel->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->nameLabel->ForeColor = System::Drawing::Color::White;
-			this->nameLabel->Location = System::Drawing::Point(115, 19);
+			this->nameLabel->Location = System::Drawing::Point(153, 23);
+			this->nameLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->nameLabel->Name = L"nameLabel";
-			this->nameLabel->Size = System::Drawing::Size(47, 17);
+			this->nameLabel->Size = System::Drawing::Size(60, 22);
 			this->nameLabel->TabIndex = 10;
 			this->nameLabel->Text = L"Name";
 			// 
@@ -281,9 +291,10 @@ namespace Bus_Service_App {
 			// 
 			this->panel1->BackColor = System::Drawing::Color::SteelBlue;
 			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(230, 26);
+			this->panel1->Location = System::Drawing::Point(307, 32);
+			this->panel1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(798, 79);
+			this->panel1->Size = System::Drawing::Size(1064, 97);
 			this->panel1->TabIndex = 11;
 			// 
 			// panel2
@@ -293,9 +304,10 @@ namespace Bus_Service_App {
 			this->panel2->Controls->Add(this->nameLabel);
 			this->panel2->Controls->Add(this->label2);
 			this->panel2->Controls->Add(this->label3);
-			this->panel2->Location = System::Drawing::Point(0, 26);
+			this->panel2->Location = System::Drawing::Point(0, 32);
+			this->panel2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(228, 79);
+			this->panel2->Size = System::Drawing::Size(304, 97);
 			this->panel2->TabIndex = 12;
 			// 
 			// panel3
@@ -303,9 +315,10 @@ namespace Bus_Service_App {
 			this->panel3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
 				static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->panel3->Controls->Add(this->cprt);
-			this->panel3->Location = System::Drawing::Point(0, 556);
+			this->panel3->Location = System::Drawing::Point(0, 684);
+			this->panel3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(1028, 55);
+			this->panel3->Size = System::Drawing::Size(1371, 68);
 			this->panel3->TabIndex = 13;
 			// 
 			// cprt
@@ -315,20 +328,22 @@ namespace Bus_Service_App {
 			this->cprt->Font = (gcnew System::Drawing::Font(L"Arial", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->cprt->ForeColor = System::Drawing::Color::White;
-			this->cprt->Location = System::Drawing::Point(262, 21);
+			this->cprt->Location = System::Drawing::Point(349, 26);
+			this->cprt->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->cprt->Name = L"cprt";
-			this->cprt->Size = System::Drawing::Size(515, 19);
+			this->cprt->Size = System::Drawing::Size(714, 24);
 			this->cprt->TabIndex = 14;
-			this->cprt->Text = L"Copyright 1997-2023 | Developed By Chomus | All Rights Reserved";
+			this->cprt->Text = L"Copyright 2022-2026 | Developed By Bus Conductors | All Rights Reserved";
 			// 
 			// walletBtn
 			// 
 			this->walletBtn->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->walletBtn->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"walletBtn.Image")));
-			this->walletBtn->Location = System::Drawing::Point(263, 372);
+			this->walletBtn->Location = System::Drawing::Point(351, 458);
+			this->walletBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->walletBtn->Name = L"walletBtn";
-			this->walletBtn->Size = System::Drawing::Size(233, 65);
+			this->walletBtn->Size = System::Drawing::Size(311, 80);
 			this->walletBtn->TabIndex = 14;
 			this->walletBtn->Text = L"  Add to Wallet";
 			this->walletBtn->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
@@ -337,10 +352,10 @@ namespace Bus_Service_App {
 			// 
 			// menu
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::LightSteelBlue;
-			this->ClientSize = System::Drawing::Size(1028, 609);
+			this->ClientSize = System::Drawing::Size(1371, 750);
 			this->Controls->Add(this->walletBtn);
 			this->Controls->Add(this->panel3);
 			this->Controls->Add(this->panel2);
@@ -353,6 +368,7 @@ namespace Bus_Service_App {
 			this->Controls->Add(this->menuStrip1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"menu";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Bus Service Application";
